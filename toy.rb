f407aef87ccd11fb36232ddb355d3481a0212799
Joe Hill <joe@jkh3.com>
2015-05-19 14:02:46 -0400

    Here's a fun Magic 8-Ball prog

---
+++ toy.rb
@@ -0,0 +1,26 @@
+responses = [
+"It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful",
+]
+
+puts "Type in any question you like, and get an answer from the Magic 8-Ball!"
+input = gets.chomp
+puts responses.sample
